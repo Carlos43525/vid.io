0e50b316195f6136fc14509c983ee97deff4d7d7
Carlos Rodriquez <carlos43525@gmail.com>
2021-03-17 10:14:49 -0400

    Cleaned up default MVC homepage and began adding customer and movie tabs populated by EF migrations.

---
+++ Vidio/Migrations/202103110602513_AddTest1.cs
@@ -0,0 +1,18 @@
+namespace Vidio.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddTest1 : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Customers", "Name", c => c.String(nullable: false, maxLength: 255));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Customers", "Name", c => c.String());
+        }
+    }
+}
